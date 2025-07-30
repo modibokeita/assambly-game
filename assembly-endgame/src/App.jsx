ad50d55fa8a6ee5c65f99cacb9b3fae459ccbf6b
Modibo Keita <modibokeita9390@gmail.com>
2025-07-31 00:08:59 +0300

    styling header

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -6,7 +6,17 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep 
+          the programming world safe from Assembly!
+        </p>
+      </header>
+      <main>
+        <section className='game-status'>
+          <h2>You win! <span>well done!🎉</span></h2>
+        </section>
+      </main>
     </>
   )
 }
